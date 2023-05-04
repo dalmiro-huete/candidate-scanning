c88f6893772731e9f5220a6d26c3ee7e56242862
Marcos Jesús Chávez Vega <m-j.chavez.v@hotmail.com>
2023-05-04 15:46:19 -0600

    make sentiment analysis and general overview as one prompt

---
+++ src/common/constants/industries.constant.ts
@@ -0,0 +1,14 @@
+export const INDUSTRIES = [
+  "Aerospace",
+  "Automotive",
+  "Electronics",
+  "Food; Beverage",
+  "Heavy Equipment",
+  "Industrial Automation",
+  "Machine Shop",
+  "Medical Devices",
+  "Metal Fabrication",
+  "Packaging",
+  "Plastics",
+  "Oil; Gas",
+];
